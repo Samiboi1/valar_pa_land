840918297587dca9b175d09d201a3767e2524eae
Samiboi1 <sam.hartelius@gmail.com>
2025-12-09 10:04:39 +0100

    btn path to script.js

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+const homeBtn = document.getElementById("googleBtn");
+const about_usBtn = document.getElementById("youtubeBtn");
+const eventsBtn = document.getElementById("googleBtn");
+const contact_usBtn = document.getElementById("googleBtn");
+
+homeBtn.addEventListener("click", () => {
+    window.location.href = "https://samiboi1.github.io/valar_pa_land/";
+});
+
+about_usBtn.addEventListener("click", () => {
+    window.location.href = "https://samiboi1.github.io/valar_pa_land/about_us.html";
+});
+
+eventsBtn.addEventListener("click", () => {
+    window.location.href = "https://samiboi1.github.io/valar_pa_land/events.html";
+});
+
+contact_usBtn.addEventListener("click", () => {
+    window.location.href = "https://samiboi1.github.io/valar_pa_land/contact_us.html";
+});

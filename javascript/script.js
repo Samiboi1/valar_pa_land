e22e610d82b11eab1c48f220d91422ba2173c72e
Samiboi1 <sam.hartelius@gmail.com>
2025-12-09 10:05:47 +0100

    header btn actually work

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
-const homeBtn = document.getElementById("googleBtn");
-const about_usBtn = document.getElementById("youtubeBtn");
-const eventsBtn = document.getElementById("googleBtn");
-const contact_usBtn = document.getElementById("googleBtn");
+const homeBtn = document.getElementById("homeBtn");
+const about_usBtn = document.getElementById("about_usBtn");
+const eventsBtn = document.getElementById("eventsBtn");
+const contact_usBtn = document.getElementById("contact_usBtn");
 
 homeBtn.addEventListener("click", () => {
     window.location.href = "https://samiboi1.github.io/valar_pa_land/";

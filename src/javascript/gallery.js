bcd72856c3e5888d3887bae3283750ed10f49849
Samiboi1 <sam.hartelius@gmail.com>
2026-05-18 14:30:19 +0200

    new samprofile

--- src/javascript/gallery.js
+++ src/javascript/gallery.js
@@ -1,5 +1,3 @@
-console.log('carousel script loaded');
-
 document.querySelectorAll('.carousel').forEach((carousel) => {
     const track = carousel.querySelector('.carousel-track');
     const slides = carousel.querySelectorAll('.slide');
@@ -8,21 +6,16 @@ document.querySelectorAll('.carousel').forEach((carousel) => {
 
     let currentIndex = 0;
 
-    console.log('found carousel', carousel.id, slides.length);
-
     function updateCarousel() {
         track.style.transform = `translateX(-${currentIndex * 100}%)`;
-        console.log('updated', carousel.id, currentIndex, track.style.transform);
     }
 
     prevBtn.addEventListener('click', () => {
-        console.log('prev clicked', carousel.id);
         currentIndex = (currentIndex - 1 + slides.length) % slides.length;
         updateCarousel();
     });
 
     nextBtn.addEventListener('click', () => {
-        console.log('next clicked', carousel.id);
         currentIndex = (currentIndex + 1) % slides.length;
         updateCarousel();
     });
